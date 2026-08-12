fc4f24af7c5683019cd53e1f867417f7669376c5
HarshitaJoshi04 <hjoshikspuk04@gmail.com>
2026-08-13 00:33:50 +0530

    changes llm services

--- backend/services/llmService.js
+++ backend/services/llmService.js
@@ -1,8 +1,5 @@
-import Groq from "groq-sdk";
+import groq from "./groqClient.js";
 
-const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY,
-});
 
 export const askLLM = async (question, context, history = []) => {
   const messages = [
